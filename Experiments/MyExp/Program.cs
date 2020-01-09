19dfbd1e6c25c6dfc475bbeb1944a592b9b5c55a
Sumanth Tirupati <sumanth.tirupati@ggktech.com>
2020-01-09 01:41:33 -0900

    exp1

---
+++ Experiments/MyExp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyExp
+{
+    class Program
+    {
+        static void f()
+        {
+            int i=3;
+            if(i==1)
+            Console.WriteLine("one");
+            else if(i==2)
+            Console.WriteLine("two");
+            else 
+            Console.WriteLine("three");
+            
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            int x=3;
+            f();
+            switch(x)
+            {
+                case 1: Console.WriteLine("one");break;
+                case 2: Console.WriteLine("two");break;
+                case 3: Console.WriteLine("three");break;
+                /*case 4: Console.WriteLine("four");break;
+                case 5: Console.WriteLine("five");break;
+                case 6: Console.WriteLine("six");break;
+                case 7: Console.WriteLine("seven");break;*/
+            }
+            Console.ReadLine();
+        }
+    }
+}
